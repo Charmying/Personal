01d2e99097a548c45b53cb55add2e01b92df794d
Charmying <charmytseng0118@gmail.com>
2022-10-02 21:58:48 +0800

    first commit

---
+++ js/send.js
@@ -0,0 +1,18 @@
+document.getElementById("send").onclick = function(){
+    alert("登錄失敗");
+};
+
+
+
+
+
+
+
+
+
+
+var el = document.querySelector(".total-block");
+
+el.addEventListener("click",function(e){
+    alert("職缺沒囉 請你下次快一點");
+},false);
